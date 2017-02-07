89b784b1b6b77fdf2885a6cb4f15bd5e8f78f2b1
Moluram <acreed3003@gmail.com>
2017-02-07 22:54:43 +0200

    GIT-1
    * Code style changes

--- task_GIT-1/src/igorhmm/currenttime/EntryPoint.java
+++ task_GIT-1/src/igorhmm/currenttime/EntryPoint.java
@@ -4,8 +4,14 @@ package igorhmm.currenttime;
  * Entry point to software to get current time,
  * No input parameters from CL are used
  */
-public class EntryPoint{
-	public static void main(String[] args){
-		System.out.println("Current time is " + CurrentTime.getTime("HH:mm"));
-	}
+public class EntryPoint {
+  public static final String TIME_FORMAT = "HH:mm";
+  public static final String STRING_CURRENT_TIME_IS = "Current time is ";
+
+  /**
+   * Starts execution of the program
+   */
+  public static void main(String[] args) {
+    System.out.println(STRING_CURRENT_TIME_IS + CurrentTime.getTime(TIME_FORMAT));
+  }
 }
